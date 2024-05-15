baf45c347b0b7af5f930d3187f74592511376103
jjclassiojr <classio.jjunior@gmail.com>
2024-05-15 16:17:50 -0300

    user list on user page

--- src/app/pages/home/components/users/users.component.ts
+++ src/app/pages/home/components/users/users.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+type user = {
+  name: string;
+  email: string;
+  role:
+    | 'Engenheiro de FE'
+    | 'Engenheiro de BE'
+    | 'Analista de Dados'
+    | 'Líder Técnico';
+  password: string;
+};
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent {
+  userList: user[];
+
+  constructor(private router: Router) {
+    this.userList = [
+      {
+        name: 'Jorge da Silva',
+        email: 'jorge@gmail.com',
+        role: 'Engenheiro de FE',
+        password: '123456',
+      },
+      {
+        name: 'Maria Oliveira',
+        email: 'maria@email.com',
+        role: 'Engenheiro de BE',
+        password: '123456',
+      },
+      {
+        name: "José da Silva",
+        email: "jose.silva@email.com",
+        role: "Analista de Dados",
+        password: "123456"
+      },
+    ];
+  }
+
+  redirectToAddUser() {
+    this.router.navigate(["/app/add-user"]);
+  }
+
+  redirectToEditUser() {
+    this.router.navigate(["/app/edit-user"]);
+  }
+
+  deleteUser(i: number){
+    this.userList.splice(i, 1);
+  }
 
 }
