94ac58b68b07263f7bd4a846bc656ed70b2399c1
RenderV <rmvmeira@gmail.com>
2024-05-15 15:07:09 -0300

    add-user and edit-user forms

--- src/app/pages/home/pages/add-user/add-user.component.ts
+++ src/app/pages/home/pages/add-user/add-user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-user',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent {
-
+  addUserForm: FormGroup;
+  constructor(private router: Router){
+    this.addUserForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      role: new FormControl(''),
+      password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]),
+    })
+  }
+  onSubmit(){
+    console.log("New user added with success!");
+    console.log(this.addUserForm.value);
+    this.router.navigate(["/app/users"]);
+  }
+  
 }
--- src/app/pages/home/pages/edit-user/edit-user.component.ts
+++ src/app/pages/home/pages/edit-user/edit-user.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
   styleUrls: ['./edit-user.component.css']
 })
-export class EditUserComponent {
 
+export class EditUserComponent{
+  editUserForm: FormGroup;
+  userToEdit: {
+    name: string,
+    email: string,
+    role: "Engenheiro de FE" | "Engenheiro de BE" | "Analista de Dados" | "Líder Técnico",
+    password: string
+  };
+
+  constructor(private router: Router){
+
+    this.userToEdit = {
+      name: "Jorge da Silva",
+      email: "jorge.silva@email.com",
+      role: "Engenheiro de FE",
+      password: "123456"
+    }
+
+    this.editUserForm = new FormGroup({
+      name: new FormControl(this.userToEdit.name, Validators.required),
+      email: new FormControl(this.userToEdit.email, [Validators.required, Validators.email]),
+      role: new FormControl(this.userToEdit.role),
+      password: new FormControl(this.userToEdit.password, [Validators.required, Validators.minLength(6), Validators.maxLength(15)]),
+    })
+  }
+  onSubmit(){
+    console.log("User edited with success!");
+    console.log(this.editUserForm.value);
+    this.router.navigate(["/app/users"]);
+  }
+  
 }
